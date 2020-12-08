aa28dd5ed3c729574a31121b58088ed8dd0e4637
Hayk <hayk.hayr3@gmail.com>
2020-12-08 23:30:16 +0400

    Add project files.

---
+++ Less05_task01/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Less05_task01
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyClass[] array = { new MyClass(), new MyClass(), new MyClass() };
+			for (int i = 0; i < array.GetLength(0); i++)
+			{
+				array[i].Method1();
+				array[i].Method2();
+			}
+
+			IInterface[] BaseArray = array;
+			for (int i = 0; i < BaseArray.GetLength(0); i++)
+			{
+				BaseArray[i].Method1();
+			}
+
+			MyClass[] DerArray = (MyClass[])BaseArray;
+			for (int i = 0; i < DerArray.GetLength(0); i++)
+			{
+				DerArray[i].Method1();
+				DerArray[i].Method2();
+			}
+
+			Console.ReadKey();
+		}
+	}
+
+	interface IInterface
+	{
+		void Method1();
+	}
+
+	class MyClass : IInterface
+	{
+		public void Method1()
+		{
+			Console.WriteLine("Method from Base");
+		}
+
+		public void Method2()
+		{
+			Console.WriteLine("Method from Derived");
+		}
+	}
+}
